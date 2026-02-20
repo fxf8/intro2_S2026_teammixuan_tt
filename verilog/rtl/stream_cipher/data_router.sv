d6d6f6ab56c9512e8896e8ed7885a481636aeadf
fxf8 <fuex9follets@gmail.com>
2026-02-20 16:12:14 -0500

    Added data router block pins

--- verilog/rtl/stream_cipher/data_router.sv
+++ verilog/rtl/stream_cipher/data_router.sv
@@ -1,12 +1,25 @@
 module data_router #(
     //parameters here
 ) (
-    input  logic clk,
+    input logic clk,
     nrst,  //clock and negative-edge reset
     //other signals here
-    output logic a
+
+    // Inputs received from reader
+    input logic [7:0] input_byte_pulsed,
+    input logic is_key_pulsed,
+    input logic input_pulse,
+
+    // Signals sent to the key storage
+    output logic [7:0] key_byte,
+    output logic key_byte_pulse,
+
+    // Signals sent to the encryption block
+    output logic [7:0] byte_pulsed,
+    output logic byte_pulse
+
 );
   //module code here
-  assign a = clk && nrst;
+  // assign a = clk && nrst;
 
 endmodule
