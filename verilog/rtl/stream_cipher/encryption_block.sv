ea559b547b8d9bb27f58a7424720fccacac1a4e3
fxf8 <fuex9follets@gmail.com>
2026-02-20 16:28:43 -0500

    Added encryption_block pins

--- verilog/rtl/stream_cipher/encryption_block.sv
+++ verilog/rtl/stream_cipher/encryption_block.sv
@@ -1,12 +1,23 @@
-module src1 #(
+module encryption_block #(
     //parameters here
 ) (
-    input  logic clk,
+    input logic clk,
     nrst,  //clock and negative-edge reset
-    //other signals here
-    output logic a
+
+    // Inputs received from the data router
+    input logic [7:0] byte_in,
+    input logic byte_in_pulse,
+
+    // Hash generator pins
+    output logic request_byte_pulse,
+    input logic [7:0] hash_byte,
+    input logic hash_byte_pulse,
+
+    // Pulsed output sent to output holder
+    output logic [7:0] encrypted_byte,
+    output logic encrypted_byte_pulse
 );
   //module code here
-  assign a = clk && nrst;
+  // assign a = clk && nrst;
 
 endmodule
