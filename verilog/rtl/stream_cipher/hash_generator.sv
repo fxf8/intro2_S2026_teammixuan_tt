2a8db4e53e027f565bbbce2427a32d1e06deab45
fxf8 <fuex9follets@gmail.com>
2026-02-20 16:22:32 -0500

    Added hash_generator pins

--- verilog/rtl/stream_cipher/hash_generator.sv
+++ verilog/rtl/stream_cipher/hash_generator.sv
@@ -1,12 +1,21 @@
-module src1 #(
+module hash_generator #(
     //parameters here
 ) (
-    input  logic clk,
+    input logic clk,
     nrst,  //clock and negative-edge reset
     //other signals here
-    output logic a
+
+    // Input receivd from key storage
+    input logic [127:0] key_memory,
+
+    // Input received from encryption block
+    input logic request_hash_byte_pulse,
+
+    // Signals sent to encryption block
+    output logic [7:0] hash_byte,
+    output logic hash_byte_pulse
 );
   //module code here
-  assign a = clk && nrst;
+  // assign a = clk && nrst;
 
 endmodule
