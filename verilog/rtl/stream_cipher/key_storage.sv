42d906326b1035cb558ff235cd9fcf20578ad41a
fxf8 <fuex9follets@gmail.com>
2026-02-20 16:15:42 -0500

    Added key_storage block pins

--- verilog/rtl/stream_cipher/key_storage.sv
+++ verilog/rtl/stream_cipher/key_storage.sv
@@ -1,12 +1,17 @@
-module src1 #(
+module key_storage #(
     //parameters here
 ) (
-    input  logic clk,
+    input logic clk,
     nrst,  //clock and negative-edge reset
-    //other signals here
-    output logic a
+
+    // Inputs received from the data router
+    input logic [7:0] key_byte,
+    input logic key_byte_pulse,
+
+    // Output sent to the hash generator
+    output logic [127:0] key_memory
 );
   //module code here
-  assign a = clk && nrst;
+  // assign a = clk && nrst;
 
 endmodule
