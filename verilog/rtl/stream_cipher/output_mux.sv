d0a5465bd93152076d92ee4121b16573ff2c4ebe
fxf8 <fuex9follets@gmail.com>
2026-02-20 16:39:52 -0500

    Added output_mux pins

--- verilog/rtl/stream_cipher/output_mux.sv
+++ verilog/rtl/stream_cipher/output_mux.sv
@@ -1,10 +1,21 @@
-module src1 #(
+module output_mux #(
     //parameters here
 ) (
+    /*
     input  logic clk,
     nrst,  //clock and negative-edge reset
-    //other signals here
-    output logic a
+    */
+
+    // Inputs from output holder
+    input logic [7:0] data_in,
+
+    // Inputs from interface fsm
+    input interface_state_t interface_state,
+
+    // Outputs sent to the output of the chip
+    output logic [7:0] data_out,
+    output logic output_byte_is_ready
+    // Note: `output_bte_is_rady` stays high until the chip user specifies the output has been read (using the output_acknowledge pin)
 );
   //module code here
   assign a = clk && nrst;
