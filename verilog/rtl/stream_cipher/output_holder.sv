4a01b8c1c6fb30ba1c9e76b6c403ace1fbad1d93
fxf8 <fuex9follets@gmail.com>
2026-02-20 16:35:19 -0500

    Added output_holder pins

--- verilog/rtl/stream_cipher/output_holder.sv
+++ verilog/rtl/stream_cipher/output_holder.sv
@@ -1,12 +1,27 @@
-module src1 #(
+// Note: The purpose of this block is to hold the pulsed output of the chip
+// for a long duration (until the user of the chip has specified that the
+// output has been read).
+
+module output_holder #(
     //parameters here
 ) (
-    input  logic clk,
+    input logic clk,
     nrst,  //clock and negative-edge reset
-    //other signals here
-    output logic a
+
+    // Inputs from encryption block
+    input logic [7:0] data_in,
+    input logic data_in_pulse,
+
+    // Inputs from interface fsm
+    input interface_state_t interface_state,
+
+    // Output sent to interface fsm
+    output logic output_is_ready,  // Note: This will likely be sent by pulse
+
+    // Output sent to output mux
+    output logic [7:0] data_out
 );
   //module code here
-  assign a = clk && nrst;
+  // assign a = clk && nrst;
 
 endmodule
