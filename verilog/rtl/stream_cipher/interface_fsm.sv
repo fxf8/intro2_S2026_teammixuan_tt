e429a149d97cf7fb6b8af2da97bfb4ef352aaa0b
fxf8 <fuex9follets@gmail.com>
2026-02-26 12:45:42 -0500

    Fixed `interface_fsm` added flipflops within block

--- verilog/rtl/stream_cipher/interface_fsm.sv
+++ verilog/rtl/stream_cipher/interface_fsm.sv
@@ -15,40 +15,44 @@ module interface_fsm #(
     input logic output_acknowledge,  // Received by the chip input pins
     input logic output_is_ready,  // This is received by the output holder block
 
-    output interface_state_t interface_state
+    output interface_state_t interface_state_out
 );
   //module code here
   // assign a = clk && nrst;
 
   interface_state_t next_interface_state;
+  interface_state_t current_interface_state;
+
+  assign interface_state_out = current_interface_state;
 
   always_ff @(posedge clk or negedge nrst) begin
     if (!nrst) begin
-      interface_state <= IDLE;
+      current_interface_state <= IDLE;
     end else begin
-      interface_state <= next_interface_state;
+      current_interface_state <= next_interface_state;
     end
   end
 
   always_comb begin
-    next_state = interface_state;
+    // The default case
+    next_interface_state = current_interface_state;
 
-    unique case (interface_state)
+    unique case (current_interface_state)
       IDLE: begin
         if (input_request) begin
-          next_state = PROCESSING;
+          next_interface_state = PROCESSING;
         end
       end
 
       PROCESSING: begin
         if (output_is_ready) begin
-          next_state = DONE;
+          next_interface_state = DONE;
         end
       end
 
       DONE: begin
         if (output_acknowledge) begin
-          next_state = IDLE;
+          next_interface_state = IDLE;
         end
       end
     endcase
