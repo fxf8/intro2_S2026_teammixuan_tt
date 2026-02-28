72e5a286957a1a2133902a85d3d7e70be6d5d6a1
fxf8 <fuex9follets@gmail.com>
2026-02-28 13:02:59 -0500

    Added explicit logic types for state enums

--- verilog/rtl/stream_cipher/types_pkg.sv
+++ verilog/rtl/stream_cipher/types_pkg.sv
@@ -1,33 +1,33 @@
 package types_pkg;
-  typedef enum {
-    I_IDLE,
-    I_PROCESSING,
-    I_DONE
+  typedef enum logic [1:0] {
+    I_IDLE = 2'b00,
+    I_PROCESSING = 2'b01,
+    I_DONE = 2'b10
   } interface_state_t;
 
-  typedef enum {
-    E_READY,
-    E_QUERRIED,
-    E_QUERRIED_AWAITING_HASH
+  typedef enum logic [1:0] {
+    E_READY = 2'b00,
+    E_QUERRIED = 2'b01,
+    E_QUERRIED_AWAITING_HASH = 2'b10
   } encryption_block_state_t;
 
-  typedef enum {
-    H_GROUND,  // The initial ground state when no hash is computed
+  typedef enum logic [2:0] {
+    H_GROUND = 3'b000,  // The initial ground state when no hash is computed
 
     // This is used for the first time a hashed byte is requested
-    H_FIRST_QUERRY,
+    H_FIRST_QUERRY = 3'b001,
 
-    H_READY,  // When the hash is computed and the marker is not at the end of the buffer
-    H_QUERRIED,  // When a hashed byte has been requested
-    H_PULSE_OUT,  // When an output is being pulsed
-    H_EXHAUSTED  // When the marker has reached the end of the buffer
+    H_READY = 3'b010,  // When the hash is computed and the marker is not at the end of the buffer
+    H_QUERRIED = 3'b011,  // When a hashed byte has been requested
+    H_PULSE_OUT = 3'b100,  // When an output is being pulsed
+    H_EXHAUSTED = 3'b101  // When the marker has reached the end of the buffer
   } hash_generator_state_t;
   // Note about `hash_generator_state`:
   // Hashes can only be requested when the hash generator is in either the
   // `GROUND` or `READY` state
 
-  typedef enum {
-    O_EMPTY,
-    O_READY
+  typedef enum logic {
+    O_EMPTY = 0,
+    O_READY = 1
   } output_holder_state_t;
 endpackage
