2383e51ff229a53abbd4b84cf3a4f853ae056f7b
fxf8 <fuex9follets@gmail.com>
2026-04-18 18:02:51 -0400

    Rewrote reader

--- verilog/rtl/stream_cipher/reader.sv
+++ verilog/rtl/stream_cipher/reader.sv
@@ -6,64 +6,42 @@ module reader (
     nrst,  //clock and negative-edge reset
 
     // Data inputs (received from chip pins)
-    input logic [7:0] input_byte,
-    input logic is_key,
-    input logic reset_hash,
+    input logic [7:0] input_byte_in,
+    input logic command_in,
 
     // Handshake Management Inputs (received from chip pins)
     input logic input_request,
 
     // FSM State (received from fsm state block)
-    input types_pkg::interface_state_t fsm_state,
+    input types_pkg::interface_state_t interface_fsm_state_in,
 
-    // Signals sent to the data router
+    // Signals sent to the command center
     output logic [7:0] input_byte_pulsed_out,  // This is the input byte that *gets pulsed*
-    output logic is_key_pulsed_out,
-    output logic input_byte_pulse_out,  // This is the pulse used to communicate the 2 outputs above
-
-    // Signal sent to the hash generator
-    output logic reset_hash_pulse
+    output logic command_pulsed_out,
+    output logic pulse_out
 );
-
-  logic [7:0] input_byte_pulsed;
-  logic is_key_pulsed;
-  logic input_byte_pulse;
-
-  assign input_byte_pulsed_out = input_byte_pulsed;
-  assign is_key_pulsed_out = is_key_pulsed;
-  assign input_byte_pulse_out = input_byte_pulse;
-
   typedef types_pkg::interface_state_t interface_state_t;
+
+  assign input_byte_pulsed_out = input_byte_in;
+  assign command_pulsed_out = command_in;
+
+  logic pulse;
+  assign pulse_out = pulse;
+  logic next_pulse;
+
+  always_comb begin
+    next_pulse = 0;
+
+    if (interface_fsm_state_in == types_pkg::I_IDLE && input_request) begin
+      next_pulse = 1;
+    end
+  end
+
   always_ff @(posedge clk or negedge nrst) begin
     if (!nrst) begin
-      input_byte_pulsed <= '0;
-      is_key_pulsed <= 1'b0;
-      input_byte_pulse <= 1'b0;
-      reset_hash_pulse <= 1'b0;
+
     end else begin
-      // First, ensure that pulses are reset to low by default
-      input_byte_pulsed <= '0;
-      is_key_pulsed <= 1'b0;
-      input_byte_pulse <= 1'b0;
-
-      if (fsm_state == types_pkg::I_IDLE && input_request) begin
-        // Only perform one action at a time (avoid overlapping actions).
-        // Different actions will have different precedences.
-        // The precedence of each actions the user can perform is as follows:
-        // 1. Reset Hash
-        // 2. Input Byte
-
-        // Reset Hash Case
-        if (reset_hash) begin
-          reset_hash_pulse <= 1'b1;
-        end  // Input Byte Case
-
-        else begin
-          input_byte_pulsed <= input_byte;
-          is_key_pulsed <= is_key;
-          input_byte_pulse <= 1'b1;
-        end
-      end
+      pulse <= next_pulse;
     end
   end
 endmodule
