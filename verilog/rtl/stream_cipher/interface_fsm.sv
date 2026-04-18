ce4d641a27b72f6e25bf9ede97ff73cfef69a546
fxf8 <fuex9follets@gmail.com>
2026-04-18 18:04:21 -0400

    Rewrote interface_fsm

--- verilog/rtl/stream_cipher/interface_fsm.sv
+++ verilog/rtl/stream_cipher/interface_fsm.sv
@@ -1,51 +1,49 @@
-// import types_pkg::interface_state_t;
-
 module interface_fsm (
     input logic clk,
     nrst,  //clock and negative-edge reset
 
     // Handshake signals
-    input logic input_request,  // Received by the chip input pins
-    input logic output_acknowledge,  // Received by the chip input pins
+    input logic input_request_in, // Received from chip input
+    input logic output_acknowledge_in, // Received from chip output
     // Received by the output holder block
-    input types_pkg::output_holder_state_t output_holder_state,
+    input types_pkg::output_holder_state_t output_holder_state_in,
 
     output types_pkg::interface_state_t interface_state_out
 );
   typedef types_pkg::interface_state_t interface_state_t;
 
   interface_state_t next_interface_state;
-  interface_state_t current_interface_state;
-
-  assign interface_state_out = current_interface_state;
+  assign interface_state_out = interface_state;
+  interface_state_t interface_state;
 
   always_ff @(posedge clk or negedge nrst) begin
     if (!nrst) begin
-      current_interface_state <= types_pkg::I_IDLE;
+      interface_state <= types_pkg::I_IDLE;
+
     end else begin
-      current_interface_state <= next_interface_state;
+      interface_state <= next_interface_state;
     end
   end
 
   always_comb begin
     // The default case
-    next_interface_state = current_interface_state;
+    next_interface_state = interface_state;
 
-    unique case (current_interface_state)
+    unique case (interface_state)
       types_pkg::I_IDLE: begin
-        if (input_request) begin
+        if (input_request_in) begin
           next_interface_state = types_pkg::I_PROCESSING;
         end
       end
 
       types_pkg::I_PROCESSING: begin
-        if (output_holder_state == types_pkg::O_READY) begin
+        if (output_holder_state_in == types_pkg::O_READY) begin
           next_interface_state = types_pkg::I_DONE;
         end
       end
 
       types_pkg::I_DONE: begin
-        if (output_acknowledge) begin
+        if (output_acknowledge_in) begin
           next_interface_state = types_pkg::I_IDLE;
         end
       end
