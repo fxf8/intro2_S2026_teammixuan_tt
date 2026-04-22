d121c6e525ca4c8105ae54433f2bb3a51db1ed04
fxf8 <fuex9follets@gmail.com>
2026-04-22 16:57:35 -0400

    Implemented encryption block interface

---
+++ verilog/rtl/stream_cipher/encryption_block_if.sv
@@ -0,0 +1,88 @@
+interface encryption_block_if;
+  logic                                     message_byte_pulse_in;
+  logic                               [7:0] message_byte_in;
+
+  logic                                     read_iv_standard_pulse;
+  logic                                     write_iv_standard_pulse_in;
+  types_pkg::chacha_setup_standard_t        write_iv_standard_in;
+
+  logic                                     read_hash_iterations_pulse;
+  logic                                     write_hash_iterations_pulse_in;
+  types_pkg::chacha_iterations_t            write_hash_iterations_in;
+
+  logic                                     read_hash_state_address_pulse;
+  logic                                     write_hash_state_address_pulse_in;
+  types_pkg::chacha_hash_state_addr_t       write_hash_state_address_in;
+
+  logic                                     read_hash_state_at_address_pulse;
+  logic                                     reset_hash_pulse_in;
+
+  // Block Counter Signals (Sent from Encryption Block)
+  logic                                     increment_block_counter_pulse;
+  logic                                     reset_block_counter_pulse;
+
+  // Outputs from Encryption Block
+  logic                                     encrypted_byte_pulse_out;
+  logic                               [7:0] encrypted_byte_out;
+
+  types_pkg::chacha_setup_standard_t        iv_standard_out;
+  types_pkg::chacha_iterations_t            hash_iterations_out;
+  types_pkg::chacha_hash_state_addr_t       hash_state_address_out;
+  logic                               [7:0] hash_state_at_address_out;
+
+  // --- Modports ---
+
+  // Command Center
+  modport command_center_port(
+      output message_byte_pulse_in,
+      output message_byte_in,
+      output write_iv_standard_pulse_in,
+      output write_iv_standard_in,
+      output write_hash_iterations_pulse_in,
+      output write_hash_iterations_in,
+      output write_hash_state_address_pulse_in,
+      output write_hash_state_address_in,
+      output read_iv_standard_pulse,
+      output read_hash_iterations_pulse,
+      output read_hash_state_address_pulse,
+      output read_hash_state_at_address_pulse,
+      output reset_hash_pulse_in
+  );
+
+  // Encryption Block
+  modport encryption_block_port(
+      input message_byte_pulse_in,
+      input message_byte_in,
+      input write_iv_standard_pulse_in,
+      input write_iv_standard_in,
+      input write_hash_iterations_pulse_in,
+      input write_hash_iterations_in,
+      input write_hash_state_address_pulse_in,
+      input write_hash_state_address_in,
+      input reset_hash_pulse_in,
+      output encrypted_byte_pulse_out,
+      output encrypted_byte_out,
+      output iv_standard_out,
+      output hash_iterations_out,
+      output hash_state_address_out,
+      output hash_state_at_address_out,
+      output increment_block_counter_pulse,
+      output reset_block_counter_pulse
+  );
+
+  // Output Holder
+  modport output_holder_port(
+      input encrypted_byte_pulse_out,
+      input encrypted_byte_out,
+      input iv_standard_out,
+      input hash_iterations_out,
+      input hash_state_address_out,
+      input hash_state_at_address_out,
+      input read_iv_standard_pulse,
+      input read_hash_iterations_pulse,
+      input read_hash_state_address_pulse,
+      input read_hash_state_at_address_pulse
+  );
+
+  modport block_counter_port(input increment_block_counter_pulse, input reset_block_counter_pulse);
+endinterface
